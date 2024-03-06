781471b8b43c0d37637f42c4856e4f00688d3213
pyunghun <564564q@naver.com>
2024-03-06 16:08:37 +0900

    123

--- src/main/java/Calculator/asd.java
+++ src/main/java/Calculator/asd.java
@@ -2,24 +2,28 @@ package Calculator;
 
 import java.util.Scanner;
 
-public class test {
+public class asd {
 
     public static void main(String[] args) {
 
         test app = new test();
 
         Scanner sc = new Scanner(System.in);
-        test test = new test();
-        System.out.print("첫번째 정수를 입력해주세요 : ");
         int first = sc.nextInt();
-
-        System.out.print("두번쨰 정수를 입력해주세요 : ");
         int second = sc.nextInt();
 
-        app.plusMethod(first, second);
 
+        app.minusMethod(first,second);
+    }
+
+    /* 곱셈 */
+    public void multi (int num1,int num2) {
+        System.out.println(num1+"*"+num2);
+        int result = num1 * num2;
+        System.out.println("결과 값은 " + result + "입니다.");
 
     }
+
 
 
     // 결과값이 음수가 나오면 예외처리
@@ -30,20 +34,20 @@ public class test {
         } else {
             System.out.println("첫 번째 값에서 두 번째 값을 뺀 값은" + result);
         }
-    }
-
-    public void plusMethod(int num1, int num2) {
-        int result = num1 + num2;
-        if (result <= 100) {
-            System.out.println(result);
-        } 
-
 
     }
 
+    /* 곱셈_예외 만들기: 결과값이 0 나오면 안됨 */
+    public void multiException(int num1, int num2) {
+
+        try {
+            System.out.println("결과값은 " + num1 * num2 + "입니다");
 
 
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+
+        }
 
 
-
-}
+    }
