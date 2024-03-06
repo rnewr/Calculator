0dc7104030fb5b2dcf8312f50c0a4cf4970e11c2
YS <sunnyskylove22@gmail.com>
2024-03-06 15:44:11 +0900

    - 곱셈/예외 추가

--- src/main/java/Calculator/test.java
+++ src/main/java/Calculator/test.java
@@ -16,6 +16,15 @@ public class test {
         app.minusMethod(first,second);
     }
 
+    /* 곱셈 */
+    public void multi (int num1,int num2) {
+        System.out.println(num1+"*"+num2);
+        int result = num1 * num2;
+        System.out.println("결과 값은 " + result + "입니다.");
+
+    }
+
+
 
     // 결과값이 음수가 나오면 예외처리
     public void minusMethod(int num1, int num2) {
@@ -26,7 +35,22 @@ public class test {
             System.out.println("첫 번째 값에서 두 번째 값을 뺀 값은" + result);
         }
 
-
     }
+
+    /* 곱셈_예외 만들기: 결과값이 0 나오면 안됨 */
+    public void multiException(int num1, int num2) {
+
+        try {
+            System.out.println("결과값은 " + num1 * num2 + "입니다");
+
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+
+        }
+
+
 }
+
 }
+
